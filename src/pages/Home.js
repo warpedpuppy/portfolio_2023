743b0f7a817db86cd5f0c4fdf8d03f5d20ec9ee8
ted <ted@warpedpuppy.com>
2023-08-30 15:37:13 -0400

    initial

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import './Home.scss';
+
+function Home() {
+	return ( 
+		<>
+		<dl>
+			<dt>challenges</dt>
+			<dd>solitaire</dd>
+			<dd>fireworks</dd>
+			<dt>gamelets</dt>
+			<dd>bounce pig, bounce</dd>
+			<dd>glitter pool</dd>
+		</dl>
+		</>
+	 );
+}
+
+export default Home;
