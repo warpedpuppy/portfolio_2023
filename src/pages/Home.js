4d078a63c34e6e18c88544d3158a57127675887c
ted <ted@warpedpuppy.com>
2023-08-31 14:41:21 -0400

    added stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,18 @@ function Home() {
 		<>
 		<dl>
 			<dt>challenges</dt>
-			<dd>solitaire</dd>
-			<dd>fireworks</dd>
+				<dd>solitaire</dd>
+				<dd>fireworks</dd>
+				<dd>maze solver</dd>
+				<dd>soduko</dd>
 			<dt>gamelets</dt>
-			<dd>bounce pig, bounce</dd>
-			<dd>glitter pool</dd>
+				<dd>bounce pig, bounce</dd>
+				<dd>dragon</dd>
+				<dd>fish</dd>
+				<dd>planet jump</dd>
+				<dd>lines</dd>
+			<dt>artsy stuff</dt>
+				<dd>glitter pool</dd>
 		</dl>
 		</>
 	 );
