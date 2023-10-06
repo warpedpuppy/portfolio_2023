10ac9b7c7cd650e9470e83b6043d8cf3f3f54aa6
ted <ted@warpedpuppy.com>
2023-10-06 11:56:52 -0400

    added drop down menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 function Menu() {
 	const navigate = useNavigate();
-	const [ page, setPage ] = useState('home')
+	const [ page, setPage ] = useState('home');
+	const [ collapsed, setCollapsed ] = useState(true)
 
 	function goToPage(e) {
 		let name = e.target.innerText.replace(/ /g, '-')
@@ -12,45 +13,20 @@ function Menu() {
 	}
 
 	return ( 
-		<nav>
-			
+		<nav className={ collapsed ? 'collapsed' : '' }>
+		<div className='hamburger' onClick={ () => setCollapsed(!collapsed)}>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
 		<ul>
 			<li onClick={ e => goToPage(e)}>home</li>
 			<li onClick={ e => goToPage(e)}>webinars</li>
 			<li onClick={ e => goToPage(e)}>challenges</li>
 			<li onClick={ e => goToPage(e)}>gamelets</li>
-			<li onClick={ e => goToPage(e)}>pretty little things</li>
+			{/* <li onClick={ e => goToPage(e)}>pretty little things</li> */}
 			<li onClick={ e => goToPage(e)}>about</li>
 		</ul>
-		<div id="submenu">
-			<div id='submenu-webinars' className={page.includes('webinars') ? 'show' : 'hide'}>
-				<ul>
-					<li><Link to="/webinars/solitaire">solitaire</Link></li>
-					<li><Link to="/webinars/svg-animations">svg animation</Link></li>
-				</ul>
-			</div>
-			<div id='submenu-challenges' className={page.includes('challenges') ? 'show' : 'hide'}>
-				<ul>
-					<li><Link to="/challenges/fireworks">fireworks</Link></li>
-					<li><Link to="/challenges/maze-solver">maze solver</Link></li>
-					<li><Link to="/challenges/soduko">soduko</Link></li>
-				</ul>
-			</div>
-			<div id='submenu-gamelets' className={page.includes('gamelets') ? 'show' : 'hide'}>
-				<ul>
-					<li><Link to="/gamelets/pig">pig</Link></li>
-					<li><Link to="/gamelets/dragon">dragon</Link></li>
-					<li><Link to="/gamelets/fish">fish</Link></li>
-					<li><Link to="/gamelets/planet-jump">planet jump</Link></li>
-					<li><Link to="/gamelets/planet-jump">lines</Link></li>
-				</ul>
-			</div>
-			<div id='submenu-pretty' className={page.includes('pretty') ? 'show' : 'hide'}>
-				<ul>
-					<li>glitter pool</li>
-				</ul>
-			</div>
-		</div>
 		</nav>
 	 );
 }
--- src/components/challenges/mazeSolver/MazeSolver.js
+++ src/components/challenges/mazeSolver/MazeSolver.js
@@ -1,12 +1,30 @@
 import './MazeSolver.scss';
 import BackButton from '../../BackButton';
+import { useEffect, useRef } from 'react';
 function MazeSolver() {
-	return ( 
-		<>
-		<BackButton />
-		<h1>maze solver</h1>
-		</>
-	 );
+	const canvasContainer = useRef(null);
+	const gameRef = useRef(null);
+
+	useEffect( () => {
+		// gameRef.current = new GameCode(canvasContainer.current);
+	}, [canvasContainer])
+
+	useEffect( () => () => gameRef.current.stop(), [] );
+
+
+	
+	return ( <>
+	<BackButton />
+	<canvas ref={canvasContainer}></canvas>
+		<div>
+			{/* <!-- <button id="switchDirections">switch directions</button> --> */}
+			<button id="addNewDisc">add new disc</button>
+			<button id="addOval">add oval</button>
+			<button id="addNewDiscInDisc">add new disc in disc</button>
+			<button id="hideDiscs">hide discs</button>
+			<button id="clear">clear</button>
+		</div>
+	</> );
 }
 
 export default MazeSolver;
