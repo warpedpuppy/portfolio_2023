7ce0d19e2ff5adce061f457c1127146ffe089cee
ted <ted@warpedpuppy.com>
2023-10-07 11:31:58 -0400

    format pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import './Home.scss';
 import BamLogo from '../components/svgs/BamLogo';
 import HomeCanvas from './home/HomeCanvas';
+import { Link } from 'react-router-dom';
 function Home() {
 	return ( 
 		<div id="home-page">
-			<BamLogo />
+			<Link to="/about"><BamLogo /></Link>
 			<HomeCanvas />
 		</div>
 	 );
