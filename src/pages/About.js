3bd30f84af0f879ddcb8be395a03308aeea26188
ted <ted@warpedpuppy.com>
2023-10-14 08:47:47 -0400

    added formatting for landing pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,13 @@ import "./About.scss";
 function About() {
   return (
     <div className="general-layout">
-      <h1>About</h1>
+		<div className="landing-page-internal-box">
+			<div className="landing-animation">
+			</div>
+      <h2>About</h2>
 	  <p>I'm Ted and I live in Maine.  I've worked in a lot of tech companies doing a lot of things.</p>
 	  <p>Now, mostly, I tinker.</p>
+	  </div>
     </div>
   );
 }
