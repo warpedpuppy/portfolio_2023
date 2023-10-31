2ddffdf7d5cb299482b573d0cf2c189f9b0934b0
ted <ted@warpedpuppy.com>
2023-10-31 11:35:42 -0400

    made pretty ring bigger

--- src/components/pretty-little-things/sparklies/code/sparkly-animation.js
+++ src/components/pretty-little-things/sparklies/code/sparkly-animation.js
@@ -7,7 +7,7 @@ class SparklyAnimation {
         fq = 250;
         starQ = 300;
         range = 100;
-        topAlpha = 0.75;
+        topAlpha = 1.5;
         colors = [ 0xff7575, 0xffb775, 0xfff175, 0xc3ff76, 0x7bffb8, 0x7de8ff, 0x799fff, 0xff93f7];
         pauseBoolean = false;
         constructor(canvas) {
