6423f09cac192cef56e44a885f3b842a4ea8bc62
ted <ted@warpedpuppy.com>
2024-02-15 11:14:52 -0500

    added home text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,10 @@ function Home() {
 	return ( 
 		<div id="home-page">
 			<Link to="/about"><BamLogo /></Link>
+			<div id="home-page-text">
+				<p>My name is Ted and I've been writing code for a while.</p>
+				<p><a href="/docs/edward_walther_resume.pdf" target='_blank'>Here's</a> my resume.</p>
+			</div>
 			<HomeCanvas />
 		</div>
 	 );
