808bd3b17be7a6886d849f4ef28f3c58a70ddf20
ted <ted@warpedpuppy.com>
2026-06-02 11:09:15 -0400

    added the larger component to the web sites page for consistency

--- src/components/webinars/WebinarsLanding.js
+++ src/components/webinars/WebinarsLanding.js
@@ -1,19 +1,19 @@
 import GeneralLandingPage from '../../layout-templates/GeneralLandingPage';
 function WebinarsLanding() {
-	return ( 
-		<GeneralLandingPage
-			title="webinars:"
-			explanatoryText={[`I've taught several webinars, and luckily there's video evidence of three of them:`]}
-			dotColor={'0xFF9900'}
-			links={
-				[
-					{link: "/webinars/solitaire", title: 'Coding Solitaire in JavaScript'},
-					{link: "/webinars/svg-animations", title: 'Animating SVGs'},
-					{link: "/webinars/web-dev", title: `Things I've Learned as a Webdev`}
-				]
-			}
-			/>
-	 );
+  return (
+    <GeneralLandingPage
+      title="webinars:"
+      explanatoryText={[`I've taught several webinars, and luckily there's video evidence of three of them:`]}
+      dotColor={'0xE8A5C6'}
+      links={
+        [
+          { link: "/webinars/solitaire", title: 'Coding Solitaire in JavaScript' },
+          { link: "/webinars/svg-animations", title: 'Animating SVGs' },
+          { link: "/webinars/web-dev", title: `Things I've Learned as a Webdev` }
+        ]
+      }
+    />
+  );
 }
 
 export default WebinarsLanding;
--- src/pages/WebSites.js
+++ src/pages/WebSites.js
@@ -1,28 +1,18 @@
 import "./WebSites.scss";
-
+import GeneralLandingPage from '../layout-templates/GeneralLandingPage';
 function WebSites() {
   return (
-    <div className="web-sites-wrapper">
-      <div className="general-layout">
-        <div className="landing-page-internal-box">
-          <h2>Web Sites</h2>
-          <ul className="web-sites-list">
-            <li>
-              <a href="https://utilspalooza.com" target="_blank" rel="noreferrer">
-                Utilspalooza
-              </a>
-              <p>A living reference of animation formulas — collision detection, trig, easing, and more — with live canvas demos and one-click code export.</p>
-            </li>
-            <li>
-              <a href="https://tryingsomething.com" target="_blank" rel="noreferrer">
-                Trying Something
-              </a>
-              <p>A rhythm training app for musicians. Practice reading sheet music, tap along to scrolling notation, and work through progressive exercises.</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <GeneralLandingPage
+      title="web sites:"
+      explanatoryText={[`I'm currently working on the following two web sites`]}
+      dotColor={'0xBBCB50'}
+      links={
+        [
+          { link: "http://tryingsomething.com", title: 'trying something' },
+          { link: "http://utilspalooza.com", title: 'utils palooza' }
+        ]
+      }
+    />
   );
 }
 
